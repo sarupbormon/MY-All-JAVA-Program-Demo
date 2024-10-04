53b50a7e6bcbfb07027d9c3bbf3a39acef36693a
Sarup <sarupbormon18@gmail.com>
2024-10-04 21:26:55 +0530

    Program-01

---
+++ AllJavaPrograms/src/AllJavaDemo/Product.java
@@ -0,0 +1,12 @@
+package AllJavaDemo;
+
+public class Product {
+	public static void main(String[] args) {
+		System.out.println("id: 101");
+		System.out.println("title: iphone");
+		System.out.println("Price: 1895 euros");
+		System.out.println("description: perfect product with image quality");
+		System.out.println("catagory: phone");
+	}
+
+}
