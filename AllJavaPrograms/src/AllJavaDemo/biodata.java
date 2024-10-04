feecb29da1a0f31e01a17c88896f7d64c615df33
Sarup <sarupbormon18@gmail.com>
2024-10-04 21:33:11 +0530

    Program-02

---
+++ AllJavaPrograms/src/AllJavaDemo/biodata.java
@@ -0,0 +1,13 @@
+package AllJavaDemo;
+
+public class biodata {
+	public static void main(String[] args) {
+		System.out.println("Name: Sarup Bormon");
+		System.out.println("Father's NAme: Nittaranjon");
+		System.out.println("Date of Birth: 15 MAy 2004");
+		System.out.println("Education Qualification: Btech CSE");
+		System.out.println("Home Address: Boligram,Matrai,Kalai,Joypurhat");
+		System.out.println("Permanent address: West Bangle,Kalkata");
+	}
+
+}
