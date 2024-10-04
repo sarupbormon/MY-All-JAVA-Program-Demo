c55759c7a28bf72978c925ed09e70f6bf260ba47
Sarup <sarupbormon18@gmail.com>
2024-10-04 22:03:12 +0530

    Program-03

---
+++ AllJavaPrograms/src/AllJavaDemo/Escapesequence.java
@@ -0,0 +1,10 @@
+package AllJavaDemo;
+
+public class Escapesequence {
+	public static void main(String[] args) {
+		System.out.println("Sarup Bormon \n +918653991057 \n Btech CSSE");
+		System.out.println("1 \t 2 \t 3");
+		System.out.println(" \"Java Programing\" ");
+	}
+
+}
