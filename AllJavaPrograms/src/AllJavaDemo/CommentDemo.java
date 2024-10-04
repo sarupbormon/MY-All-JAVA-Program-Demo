f6daff150ba0d38d70ffb8c77040620fb3c8ea26
Sarup <sarupbormon18@gmail.com>
2024-10-04 22:21:11 +0530

    Program-04

---
+++ AllJavaPrograms/src/AllJavaDemo/CommentDemo.java
@@ -0,0 +1,19 @@
+package AllJavaDemo;
+
+
+/* Sarup Bormon
+   Bio Data
+   date:04.10.2024  
+ */
+
+public class CommentDemo {
+	public static void main(String[] args) {
+		
+		//display my name 
+		System.out.println("Sarup Bormon"); 
+		
+		//display my name
+		System.out.println("Age:15 May 2004");
+	}
+
+}
