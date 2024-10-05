1ba763041f513b32d858abfc5aa94b7efa5cee2a
Sarup <sarupbormon18@gmail.com>
2024-10-06 01:04:53 +0530

    Program-20

---
+++ AllJavaPrograms/src/AllJavaDemo/PreincreamentDemo.java
@@ -0,0 +1,16 @@
+package AllJavaDemo;
+
+public class PreincreamentDemo {
+	public static void main(String[] args) {
+		
+		int x = 25;
+		int y;
+		y = ++x;  //Pre increament x= 26
+		System.out.println("y = "+y);;
+		y = x;
+		System.out.println("y = "+x);
+		 
+	
+    }
+
+}
