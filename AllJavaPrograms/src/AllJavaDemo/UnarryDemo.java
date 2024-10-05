315e1bf441fc4ae0ea6bf780383565fb226ffd98
Sarup <sarupbormon18@gmail.com>
2024-10-06 00:40:47 +0530

    Program-18

---
+++ AllJavaPrograms/src/AllJavaDemo/UnarryDemo.java
@@ -0,0 +1,17 @@
+package AllJavaDemo;
+
+public class UnarryDemo {
+	public static void main(String[] args) {
+		int x = -10;
+		int result;
+		 
+		result = +x;  //-10
+		System.out.println("Result = "+result);
+		
+		result = -x;  //+10
+		System.out.println("Result = "+result);
+		
+		
+	}
+
+}
