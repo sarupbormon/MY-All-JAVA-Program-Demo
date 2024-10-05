267ac980ab91c8f5778efb7c04da41c08ee21370
Sarup <sarupbormon18@gmail.com>
2024-10-06 00:28:46 +0530

    Program-17

---
+++ AllJavaPrograms/src/AllJavaDemo/TemperatureDemo.java
@@ -0,0 +1,17 @@
+package AllJavaDemo;
+import java.util.Scanner;
+
+public class TemperatureDemo {
+   public static void main(String[] args) {
+	
+	   Scanner input = new Scanner(System.in);
+	   double cels,farn;
+	   
+	   System.out.println("Celcious = ");
+	   cels = input.nextDouble();
+	   
+	   farn = 1.8*cels + 32;
+	   System.out.println("Farenheit = "+farn);
+	   
+   } 
+}
