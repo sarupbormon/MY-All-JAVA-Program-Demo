a635094aaf89673f741342399f86d1853886c129
Sarup <sarupbormon18@gmail.com>
2024-10-05 13:57:35 +0530

    Program-10

---
+++ AllJavaPrograms/src/AllJavaDemo/DoubleUserInput.java
@@ -0,0 +1,15 @@
+package AllJavaDemo;
+import java.util.Scanner;
+
+public class DoubleUserInput {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		double num1;
+		System.out.print("Enter any double value : ");
+		num1 = input.nextDouble();
+		System.out.println("Enetr your Value :"+num1);
+		
+		
+	}
+
+}
