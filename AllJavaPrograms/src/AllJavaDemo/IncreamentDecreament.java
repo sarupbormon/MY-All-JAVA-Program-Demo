b60f5f926ce5e63efa1e4624ece3dda59b9b3ac4
Sarup <sarupbormon18@gmail.com>
2024-10-06 00:58:29 +0530

    program-19

---
+++ AllJavaPrograms/src/AllJavaDemo/IncreamentDecreament.java
@@ -0,0 +1,15 @@
+package AllJavaDemo;
+
+public class IncreamentDecreament {
+	public static void main(String[] args) {
+		
+		
+		int x = 25;
+		int y;
+		y = x++; //post increament
+		System.out.println("y = "+y);
+		 y = x;
+		 System.out.println("y = "+y);
+	}
+
+}
