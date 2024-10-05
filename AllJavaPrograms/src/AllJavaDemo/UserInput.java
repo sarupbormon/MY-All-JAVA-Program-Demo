186123d140469364f7e0fb9d6c8747e25565c9bd
Sarup <sarupbormon18@gmail.com>
2024-10-05 13:42:57 +0530

    Program-08

---
+++ AllJavaPrograms/src/AllJavaDemo/UserInput.java
@@ -0,0 +1,22 @@
+package AllJavaDemo;
+
+import java.util.Scanner;
+
+public class UserInput {
+	
+	public static void main(String[] args) {
+		
+		
+		Scanner input = new Scanner(System.in);
+		int number;
+		System.out.print("Eneter any Number : ");
+		
+		number = input.nextInt();
+		System.out.println("Enter your number = "+number);
+		
+		
+		
+		
+	}
+
+}
