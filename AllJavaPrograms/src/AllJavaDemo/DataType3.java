d8e3538138f2fca8065c656ae087318d06e26d19
Sarup <sarupbormon18@gmail.com>
2024-10-05 13:10:10 +0530

    Program-07

---
+++ AllJavaPrograms/src/AllJavaDemo/DataType3.java
@@ -0,0 +1,26 @@
+package AllJavaDemo;
+
+public class DataType3 {
+	public static void main(String[] args) {
+		
+		//Use to the print of formate specifier
+		
+		boolean b = true;
+		char c = 'a';
+		short s = 32677;
+		int i = 126587;
+		float f = 10.2f;
+		double d = 10.2;
+		System.out.printf("Boolean b = %b\n",b);
+		System.out.printf("Character c = %c\n",c);
+		System.out.printf("Short s = %d\n",s);
+		System.out.printf("Integer i = %d\n",i);
+		System.out.printf("Float f = %.2f\n",f);
+		System.out.printf("Double d = %.3f\n",d);
+		
+		
+		
+		
+	}
+
+}
