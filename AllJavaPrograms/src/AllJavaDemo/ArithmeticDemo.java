e1fd5d5157e895063cf03c0194f83a238e2ee382
Sarup <sarupbormon18@gmail.com>
2024-10-05 20:35:43 +0530

    Program-12

---
+++ AllJavaPrograms/src/AllJavaDemo/ArithmeticDemo.java
@@ -0,0 +1,22 @@
+package AllJavaDemo;
+
+public class ArithmeticDemo {
+	public static void main(String[] args) {
+            
+		int num1,num2,result;
+		num1 = 20;
+		num2 = 10;
+		result = num1 + num2;
+		System.out.println("Sum = "+result);
+		result =num1 - num2;
+		System.out.println("Sub = "+result);
+		result =num1 * num2;
+		System.out.println("Multiplication = "+result);
+		result =num1 / num2;
+		System.out.println("Div = "+result);
+		result =num1 % num2;
+		System.out.println("Remainder = "+result);
+		
+	}
+
+}
