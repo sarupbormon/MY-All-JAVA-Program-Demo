e05cad9afaa122f88138093f2a98fa34ea26d634
Sarup <sarupbormon18@gmail.com>
2024-10-05 12:42:02 +0530

    Program-06

---
+++ AllJavaPrograms/src/AllJavaDemo/DataType2.java
@@ -0,0 +1,25 @@
+package AllJavaDemo;
+
+public class DataType2 {
+	public static void main(String[] args) {
+		//step 3: Declare variable
+		
+		 int id ;
+		 String title,price,description,category;
+		 //asssign the following data
+		 id = 101;
+		 title = "iphone15";
+		 price ="1895 euros";
+		 description = "perfect product with best image quality";
+		 category = "phone";
+		 
+		//print the data
+		System.out.println("ID :"+id);
+		System.out.println("TITLE: "+title);
+		System.out.println("PRICE: "+price);
+		System.out.println("DESCRIPTION: "+description);
+		System.out.println("CATEGORY: "+category);
+		
+	}
+
+}
