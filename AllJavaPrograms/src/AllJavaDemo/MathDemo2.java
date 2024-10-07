b8126828dc7880997af96263991a958ace17c8ea
Sarup <sarupbormon18@gmail.com>
2024-10-07 20:06:50 +0530

    Program-25

---
+++ AllJavaPrograms/src/AllJavaDemo/MathDemo2.java
@@ -0,0 +1,23 @@
+package AllJavaDemo;
+
+public class MathDemo2 {
+	public static void main(String[] args) {
+		
+		int x = 20;
+		int y = 10;
+		
+		int absolute = Math.abs(y);
+		System.out.println("Absolute of y = "+absolute);
+		
+		double power = Math.pow(x, y);
+		System.out.println("x to the power y = "+power);
+		
+		int round = Math.round(8.8f);
+		System.out.println("Round of 8.8 = "+round);
+		
+		double pi = Math.PI;
+		System.out.println("pi = "+pi);
+		
+	}
+
+}
