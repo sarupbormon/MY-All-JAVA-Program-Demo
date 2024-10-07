f95fc54cc90496f32b7eb5753f04fa1c6e61b2a3
Sarup <sarupbormon18@gmail.com>
2024-10-07 19:53:42 +0530

    Program-24

---
+++ AllJavaPrograms/src/AllJavaDemo/MathDemo.java
@@ -0,0 +1,21 @@
+package AllJavaDemo;
+
+public class MathDemo {
+	
+	public static void main(String[] args) {
+		
+		int x = 20;
+		int y = 10;
+		
+		int max = Math.max(x, y);
+		
+		System.out.println("Maximum = "+max);
+		
+		int min = Math.min(x , y);
+		System.out.println("Mainimum = "+min);
+		 
+		
+		
+	}
+
+}
